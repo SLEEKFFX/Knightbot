b1815bd8ac32f4e823e05b5b31b7dd324db6c322
SLEEKFFX <166394789+SLEEKFFX@users.noreply.github.com>
2024-12-01 10:42:35 +0100

    settings.js

--- settings.js
+++ settings.js
@@ -2,11 +2,11 @@ const settings = {
   packname: 'My Sticker Pack',
   author: 'My Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'sleekffx', // Your name
+  ownerNumber: '08140266965', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "bow to sleek.",
   version: "1.0.0",
 };
 
